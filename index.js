348521fa25468bdd1b890830ec1b30b2a7615b53
redebron20 <redebron20@gmail.com>
2021-06-24 13:52:13 -0400

    commit tree 0c42ff3bc68a341b6f0dd0b3a9805dd6729a629a

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function saturdayFun(activity=`roller-skate`) {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity=`go to the office`) {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(param=`*`) {
+    return function(adj=`special`) {
+        return `You are ${param}${adj}${param}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b;
+    },
+    multiply: function(a,b) {
+        return a * b;
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
+}
+function actionApplyer(int, arr) {
+    let a = int
+
+    for(let i = 0; i < arr.length; i++) {
+        a = arr[i](a)
+    } 
+    
+    return a
+}
